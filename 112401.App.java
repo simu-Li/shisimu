2824e158040d74718b9a994eb12d7cdf6f16de72
simu-Li <74776007+simu-Li@users.noreply.github.com>
2020-11-24 21:06:22 +0800

    Create 112401.App.java
    鼠标监听和键盘模块01

---
+++ 112401.App.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class App {
+    JPanel myPanel=new JPanel();
+    JLabel label_txt=new JLabel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+    //构造方法
+   public App() {
+        textArea_B.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+                if ((e.getKeyCode() == KeyEvent.VK_UP)) {
+                    textArea_B.append("欢迎进入上流社会\n");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN){
+                    textArea_B.setText("");
+                }
+            }
+        });
+    }
+    //显示窗体方法
+    void go(){
+        textArea_A.setBounds(100,100,300,300);
+        textArea_A.setBackground(Color.pink);
+        textArea_B.setBounds(100,500,300,300);
+        textArea_B.setBackground(Color.orange);
+        myPanel.add(textArea_A);
+        myPanel.add(textArea_B);
+
+        JFrame frame = new JFrame("TestGUI");
+        myPanel.setLayout(null);
+        myPanel.add(label_txt);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,512,900);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new App().go();
+    }
+}
