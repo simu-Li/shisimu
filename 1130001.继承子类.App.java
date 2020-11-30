c8685f2dbacbd734cb3cb58eeef0e494048cb5c3
simu-Li <74776007+simu-Li@users.noreply.github.com>
2020-11-30 15:14:39 +0800

    Create 1130001.继承子类.App.java

---
+++ 1130001.继承子类.App.java
@@ -0,0 +1,14 @@
+package pac;
+
+import java.util.Arrays;
+
+public class App extends R{
+
+	public static void main(String[] args) {
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d[0][2]+","+array2d[1][0]);
+		
+	}
+
+}
