65e8b5153fbfed44fa0dc09d1e653ad994cc3d21
simu-Li <74776007+simu-Li@users.noreply.github.com>
2020-11-30 15:30:46 +0800

    Update 1130001.继承子类.App.java

--- 1130001.继承子类.App.java
+++ 1130001.继承子类.App.java
@@ -7,8 +7,15 @@ public class App extends R{
 	public static void main(String[] args) {
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		System.out.println(array2d[0][2]+","+array2d[1][0]);
 		
+		for(int row = 0;row < array2d.length;row++){
+			for(int cloumn = 0;cloumn < array2d[0].length;cloumn++){
+				System.out.print(array2d[row][cloumn]+",");
+			}
+			System.out.println();
+		}
+		   
 	}
 
 }
+
