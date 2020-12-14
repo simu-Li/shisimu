359099b7aa4bc9d524c4dd2e87cb0bbd463e1868
simu-Li <74776007+simu-Li@users.noreply.github.com>
2020-12-14 15:12:17 +0800

    Create 121401.奇数求和.App.java

---
+++ 121401.奇数求和.App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        long  num = 0;
+        for(long  i=1;i<=2147483647;){
+            num+=i;
+            i+=2;
+        }
+        System.out.println("奇数相加结果为："+num);
+    }
+}
