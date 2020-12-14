590df9cb3521ed54c22f7c8c97f4ecb08a812187
simu-Li <74776007+simu-Li@users.noreply.github.com>
2020-12-14 15:38:18 +0800

    Update 121402.斐波那契数列第n项.App.java

--- 121402.斐波那契数列第n项.App.java
+++ 121402.斐波那契数列第n项.App.java
@@ -1,1 +1,23 @@
-you dai geng xin
+package pac;
+
+import java.util.Scanner;
+
+public class App {
+public static void main(String[] args) {
+  int i = 0;
+	Scanner scanner=new Scanner(System.in);
+	i=scanner.nextInt();
+		 
+		        System.out.println("第"+i+"项为："+getNum(i));
+		    }
+		    
+			private static int getNum(int i){
+		        if (i<=0){
+		            return -1;
+		        }else if (i==1||i==2){
+		            return 1;
+		        }else {
+		            return getNum(i-1)+getNum(i-2);
+		        }
+		    }
+}
